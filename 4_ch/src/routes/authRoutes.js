7ac491316c93ead981fe3aef971cf5e0f7b169e4
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2024-12-15 18:19:37 +0500

    update to db to prisma

--- 4_ch/src/routes/authRoutes.js
+++ 4_ch/src/routes/authRoutes.js
@@ -2,32 +2,37 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import db from "../db.js";
+import prisma from "../prismaClient.js";
 const router = express.Router();
 router.use(express.json());
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { username, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 9);
   //   console.log(hashedPassword);
   //   console.log(username, password);
   try {
-    const insertUser = db.prepare(
-      `INSERT INTO users (username, password) VALUES(?,?)`
-    );
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
 
     const result = insertUser.run(username, hashedPassword);
     // add the first todo for the user
     const defaultTodo = `hello!, add your first todo`;
+    await prisma.todo.create({
+      data: { task: defaultTodo, userId: user.id },
+    });
     const insertTodo = db.prepare(
       `INSERT INTO todos (user_id,task) VALUES (?,?)`
     );
     insertTodo.run(result.lastInsertRowid, defaultTodo);
     //create a token
-    const token = jwt.sign(
-      { id: result.lastInsertRowid },
-      process.env.JWT_SECRET,
-      { expiresIn: "24h" }
-    );
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
     res.json({ token });
   } catch (error) {
     console.log(error.message);
@@ -35,12 +40,15 @@ router.post("/register", (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`);
-    const user = getUser.get(username);
+    const user = await prismama.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
     //gaurd clause, if we cannot find a user, we return out.
     if (!user) {
       return res.status(404).send({ message: "username/email not found" });
@@ -50,12 +58,12 @@ router.post("/login", (req, res) => {
     if (!passwordIsValid) {
       return res.status(401).send({ message: "wrong password..." });
     }
-    console.log(user)
+    console.log(user);
     //if user exits and password is correct, we have a successfull authentication
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: "24h",
     });
-    res.json(token)
+    res.json(token);
   } catch (error) {
     console.log(error.message);
     res.sendStatus(503);
